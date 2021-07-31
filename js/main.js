e77de61dd53923ac31e610852fbaf62e2b26d628
Rafiur Rahman <rrfoundary@gmail.com>
2021-07-31 17:54:55 +0600

    Tracking page is ready after all

--- js/main.js
+++ js/main.js
@@ -17,4 +17,26 @@ $(document).ready(function () {
   document
     .querySelector(".iti.iti--allow-dropdown .form-control")
     .classList.add("w-100");
+  // Tracking page
+  var input = document.querySelector("#telephone-track");
+  intlTelInput(input, {
+    // options here
+    initialCountry: "bd",
+    customPlaceholder: function (
+      selectedCountryPlaceholder,
+      selectedCountryData
+    ) {
+      return "";
+    },
+    separateDialCode: true,
+    utilsScript: "js/utils.js",
+  });
+  document
+    .querySelector(".section-tracking-download .iti.iti--allow-dropdown")
+    .classList.add("w-65");
+  document
+    .querySelector(
+      ".section-tracking-download .iti.iti--allow-dropdown .form-control"
+    )
+    .classList.add("w-100");
 });
