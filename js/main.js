00751cb69bec7a22511f9e4b60c3a05d5a097862
Rafiur Rahman <rrfoundary@gmail.com>
2021-07-31 08:34:21 +0600

    about page ready

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
-// Vanilla Javascript
-var input = document.querySelector("#telephone");
-window.intlTelInput(input, {
-  // options here
+$(document).ready(function () {
+  // Vanilla Javascript
+  var input = document.querySelector("#telephone");
+  intlTelInput(input, {
+    // options here
+    initialCountry: "bd",
+    customPlaceholder: function (
+      selectedCountryPlaceholder,
+      selectedCountryData
+    ) {
+      return "";
+    },
+    utilsScript: "js/utils.js",
+  });
+  document.querySelector(".iti.iti--allow-dropdown").classList.add("w-65");
+  document
+    .querySelector(".iti.iti--allow-dropdown .form-control")
+    .classList.add("w-100");
 });
